feb89b5b23dffec30ce0365a1f5730856b9666e0
Vinicio <calamot51@gmail.com>
2024-01-29 17:44:47 +0100

    robot hablando ok

--- get_headlines.sh
+++ get_headlines.sh
@@ -1,14 +1,5 @@
-#!/bin/bash
-
-# Define la URL del portal de noticias
-url="https://elpais.com/"
-
-# Utiliza wget para descargar el código fuente de la página
-wget -q -O - "$url" > elpais_source.html
-
-# Busca patrones que podrían indicar titulares. Este es un ejemplo básico;
-# necesitarás ajustar los patrones de búsqueda según lo que encuentres en el sitio.
-# Este comando busca ejemplos de etiquetas <h1>, <h2>, y <a> que podrían contener titulares.
+#-P: Esta opción le dice a grep que interprete el patrón de búsqueda como una expresión regular de Perl(expresiones regulares mas optimizadas). 
+#Las expresiones regulares de Perl son más potentes y flexibles que las expresiones regulares básicas que grep utiliza por defecto.
 grep -oP '(<h1[^>]*>.*?</h1>|<h2[^>]*>.*?</h2>|<a[^>]*class="[^"]*titulo[^"]*"[^>]*>.*?</a>)' elpais_source.html > headlines.txt
 
 # Muestra los resultados
